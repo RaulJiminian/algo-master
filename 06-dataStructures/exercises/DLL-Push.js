9a3274fad006680d489cdbde8ee598c60da598ac
Raul Jiminian <raul.e.jiminian@gmail.com>
2021-01-31 14:03:01 -0500

    added DLL exercises folder, push method included

---
+++ 06-dataStructures/exercises/DLL-Push.js
@@ -0,0 +1,93 @@
+// DLL Push
+// Implement the following on the Doubly Linked List class
+
+// push()
+
+// This function should accept a value
+// It will add a node to the end of the DLL with the value passed to the function
+
+// ------------------------------------
+//       -- STARTER CODE --
+// ------------------------------------
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//     this.prev = null;
+//   }
+// }
+
+// class DoublyLinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.tail = null;
+//     this.length = 0;
+//   }
+
+//   push() {}
+// }
+
+// Example (input/output):
+// let dll = new DoublyLinkedList();
+// dll.push(5) // dll
+// dll.length // 1
+// dll.head.val // 5
+// dll.tail.val // 5
+// dll.head.prev // null
+// dll.push(10) // dll
+// dll.length // 2
+// dll.head.val // 5
+// dll.head.next.val // 10
+// dll.tail.val // 10
+// dll.push(15) // dll
+// dll.length // 3
+// dll.head.val // 5
+// dll.tail.val // 15
+// dll.tail.prev.val // 10
+// dll.head.next.next.val // 15
+
+// dll.pop().val // 15
+// dll.length // 2
+// dll.pop().val // 10
+// dll.length // 1
+// dll.pop().val // 5
+// dll.length // 0
+// dll.pop() // undefined
+// dll.length // 0
+
+// ------------------------------------
+//       -- SOLUTION BELOW --
+// ------------------------------------
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      let oldtail = this.tail;
+      oldtail.next = newNode;
+      newNode.prev = oldtail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+}
